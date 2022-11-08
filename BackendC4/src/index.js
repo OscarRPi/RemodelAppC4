f1109260be82bc8e407799d752b7190d7f7637f1
thencabito <orpinedal@unal.edu.co>
2022-11-08 21:47:21 +0100

    Areglando error de typo

--- BackendC4/src/index.js
+++ BackendC4/src/index.js
@@ -5,7 +5,7 @@ require('./database')
 
 async function main(){
     await app.listen(app.get('port'))
-    console.log('El servidor esta corriendo en le puerto ' + app.get('port'))
+    console.log('El servidor esta corriendo en el puerto ' + app.get('port'))
 }
 
 main()
