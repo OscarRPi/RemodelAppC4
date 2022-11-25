126e98e2de939bb731629dc025a2f116edf56002
jnieves64 <110790015+jnieves64@users.noreply.github.com>
2022-11-25 14:58:15 -0500

    Frontend 4
    Metodo post para logueo en zona admin

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,30 +1,57 @@
 import '../pages/Login.css'
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Login(){
 
+    const initialState = {username: "", password: ""};
+
+    const [user, setUser] = useState(initialState);
+
+    const handleInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
     const navigate = useNavigate();
 
-    const handleSubmit= (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('hola');
-        navigate('/dashboard');
-    }
+        try {
+            let res = await fetch('http://localhost:30000/users/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "username": String(user.username).trim(),
+                    "password": String(user.password).trim()
+                })
+            })
+            const data = await res.json();
+            if (data.username === "admin") {
+                navigate('/dashboard')
+            } else {
+                console.log('Datos Incorrectos')
+            }
+        } catch (error) {
+            console.log(error);
+        };
+    };
 
     return (
         <div class="body-login">
-            <form className='container-login centrar'>
+            <form onSubmit={handleSubmit} className='container-login centrar'>
                 <h1>ADMIN</h1>
                 <div class="formgrupo centrar">
                     <label for="username">USUARIO</label>
-                    <input type="text" name="username" id="username" />
+                    <input onChange={handleInputChange} value={user.username} type="text" name="username" id="username" />
                 </div>
                 <div class="formgrupo centrar mb-4">
                     <label for="password">CONTRASEÑA</label>
-                    <input type="password" name="password" id="password" />
+                    <input onChange={handleInputChange} value={user.password} type="password" name="password" id="password" />
                 </div>
                 <div class="boton centrar mb-4">
-                <button onClick={handleSubmit} type="button" id="logear" class="mt-2">INGRESAR</button>
+                <button type="submit" id="logear" class="mt-2">INGRESAR</button>
                 </div>
             </form>
             <h2 class="mt-5 mb-5">REMODEL<span class="bold">APP</span></h2>
